1ede694e31708d5295a85f4a950e7303d10ac4fa
twarbelow <taija.warbelow@gmail.com>
2021-07-19 08:54:10 -0700

    expand test

--- spec/mid_mod_spec.rb
+++ spec/mid_mod_spec.rb
@@ -2,43 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'as a user' do
   it 'I can search foods' do
-    # be a user
     visit root_path
     fill_in :q, with: "sweet potatoes"
     click_button "Search"
     expect(current_path).to eq("/foods")
 
-    # it 'and see the total numbers of search results' do
-    #   expect(page).to have_content("Total Results: ")
-    # end
-    # it 'and see 10 foods containing my search term' do
-    #   # within
-    #   #   count == 10
-    #   # end
-    # end
-    # it 'for each food I should see details' do
-    #   # withing the first result
-    #   expect(page).to have_content("GTIN/UPC Code: ")
-    #   expect(page).to have_content("Description: ")
-    #   expect(page).to have_content("Brand Owner: ")
-    #   expect(page).to have_content("Ingredients: ")
-    # end
+    it 'and see the total numbers of search results' do
+      expect(page).to have_content("Total Results: ")
+    end
+    it 'and see 10 foods containing my search term' do
+      expect(page).to have_css('single-result', count: 10)
+    end
+    it 'for each food I should see details' do
+      within(".single_result", match: :first) do
+        expect(page).to have_content("GTIN/UPC Code: ")
+        expect(page).to have_content("Description: ")
+        expect(page).to have_content("Brand Owner: ")
+        expect(page).to have_content("Ingredients: ")
+      end
+    end
   end
 end
-
-
-
-# As a user,
-# When I visit "/"
-# And I fill in the search form with "sweet potatoes"
-# (Note: Use the existing search form)
-# And I click "Search"
-# Then I should be on page "/foods"
-# Then I should see a total of the number of items returned by the search.
-# (sweet potatoes should find more than 30,000 results)
-# Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
-# And for each of the foods I should see:
-# - The food's GTIN/UPC code
-# - The food's description
-# - The food's Brand Owner
-# - The food's ingredients
