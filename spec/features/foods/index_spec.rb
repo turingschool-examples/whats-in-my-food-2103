aac46a4c3a51463d65141c443fb286c512b84b05
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-19 10:00:02 -0700

    Add in search to direct to correct search path

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Foods Index Page' do
   it 'can show the food I searched for' do
-    visit foods_path
+    visit root_path
 
+    fill_in "q", with: "sweet potatoes"
+    click_button "Search"
+
+    visit foods_path
     expect(page).to have_content("items found")
   end
 end
