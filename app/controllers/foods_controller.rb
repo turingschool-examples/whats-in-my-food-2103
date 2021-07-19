dab1f1bf23e55f5478896265ce924c843a886744
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-19 09:12:42 -0700

    Create foods controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,34 @@
+class FoodsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  #private
+  #def _params
+    #params.permit(:)
+  #end
+end
