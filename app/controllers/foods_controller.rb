83763c61a281bfaff45ce9a03e7099e334215bb0
Zach T <ztrokey@gmail.com>
2021-07-19 10:42:12 -0600

    clean out commented code

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,6 +8,5 @@ class FoodsController < ApplicationController
     )
     response = conn.get("?query=#{params[:q]}&pageSize=10")
     @results = JSON.parse(response.body, symbolize_names: true)
-    # require 'pry'; binding.pry
   end
 end
