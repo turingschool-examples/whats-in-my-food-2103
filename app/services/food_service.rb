80ee64179c1e55c324b883f3e5dd2629e19572a1
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-19 09:38:21 -0600

    Implement consuming food api data

---
+++ app/services/food_service.rb
@@ -0,0 +1,14 @@
+class FoodService
+  def self.get_food_data(search_food)
+    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search") do |req| # ?stateCode=#{state}&api_key=#{ENV['nps_api']}
+      req.params['api_key'] = ENV['food_api']
+      req.params[:query] = search_food
+    end
+
+    parse_json(response)
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
