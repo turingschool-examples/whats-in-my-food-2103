6d66783af5d98e778b3aa331ea58c44a7b8fb08b
Zach T <ztrokey@gmail.com>
2021-07-19 10:17:33 -0600

    add api call to controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,12 @@
 class FoodsController < ApplicationController
   def index
-    # require 'pry'; binding.pry
+    conn = Faraday.new(
+      url: 'https://api.nal.usda.gov/fdc/v1/foods/search',
+      params: {
+        api_key: ENV['food_data_key']
+      }
+    )
+    response = conn.get("?query=#{params[:q]}")
+    parsed = JSON.parse(response.body, symbolize_names: true)
   end
 end
