2b7d12a128fbe009a3bb7dd324d48c8a4fa3f9b0
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-07-19 09:46:01 -0600

    create index page

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'foods index page' do
+
+  describe "search sends you to food inex page" do
+
+    visit '/'
+
+    fill_in "q", with: "Sweet potato"
+    click_button "Search"
+
+    expect(current_path).to eq("/foods")
+  end
+
+
+  describe "search sends you to food inex page" do
+
+    visit '/'
+
+    fill_in "q", with: "Sweet potato"
+    click_button "Search"
+
+    expect(current_path).to eq("/foods")
+
+    
+  end
+end
