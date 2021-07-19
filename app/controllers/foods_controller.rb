f8f00bc11d85ed510a7489beb33de6b2cbcb941a
Zach T <ztrokey@gmail.com>
2021-07-19 10:41:02 -0600

    correct api call

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,7 +6,8 @@ class FoodsController < ApplicationController
         api_key: ENV['food_data_key']
       }
     )
-    response = conn.get("?query=#{params[:q]}")
-    parsed = JSON.parse(response.body, symbolize_names: true)
+    response = conn.get("?query=#{params[:q]}&pageSize=10")
+    @results = JSON.parse(response.body, symbolize_names: true)
+    # require 'pry'; binding.pry
   end
 end
