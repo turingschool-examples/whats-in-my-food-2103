629269cd8cead4a13cdca44cbb11731b9f2397ac
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-07-19 18:29:46 +0200

    Updates tests

--- app/services/foods_service.rb
+++ app/services/foods_service.rb
@@ -1,7 +1,7 @@
 class FoodsService
 
   def self.foods_search(ingredient)
-    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['API_KEY']}&query%3D=#{ingredient}")
+    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['API_KEY']}&query=#{ingredient}")
     json = JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/foods_service_spec.rb
+++ spec/services/foods_service_spec.rb
@@ -4,10 +4,22 @@ RSpec.describe FoodsService do
   context "class methods" do
     context "#foods_by_ingredient" do
       it "returns food data", :vcr do
-        search = FoodsService.foods_search("Cheese")
+        search = FoodsService.foods_search("Sweet Potatoes")
         expect(search).to be_a Hash
         expect(search[:foods]).to be_an Array
         food_data = search[:foods].first
+
+        expect(food_data).to have_key :gtinUpc
+        expect(food_data[:gtinUpc]).to be_a(String)
+
+        expect(food_data).to have_key :description
+        expect(food_data[:description]).to be_a(String)
+
+        expect(food_data).to have_key :brandOwner
+        expect(food_data[:brandOwner]).to be_a(String)
+
+        expect(food_data).to have_key :ingredients
+        expect(food_data[:ingredients]).to be_a(String)
       end
     end
   end
