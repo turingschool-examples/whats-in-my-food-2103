87e4a96707fc5bec5b1deff5193dc9d00ff08a7f
Zach Green <zachjamesgreen@gmail.com>
2021-07-19 10:31:57 -0600

    limit to 10 items

--- app/lib/foods.rb
+++ app/lib/foods.rb
@@ -1,10 +1,14 @@
 class Foods
-  attr_reader :foods, :total_hits
+  attr_reader :total_hits
 
   def initialize(foods)
     @foods = foods['foods'].map do |food|
       Food.new(food)
     end
     @total_hits = foods['totalHits']
+  end
+
+  def foods(num = 10)
+    @foods.first(num)
   end
 end
