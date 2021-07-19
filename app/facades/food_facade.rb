852204229b47d7da229c0e2721e1300e2481bbdc
twarbelow <taija.warbelow@gmail.com>
2021-07-19 09:28:35 -0700

    fix for class method syntax

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,5 +1,5 @@
 class FoodFacade
-  def search(search_term)
+  def self.search(search_term)
     response = FoodService.search_foods(search_term)
     response.map do |result|
       FoodPoro.new(result)
--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -6,7 +6,7 @@ describe 'FoodFacade' do
 
     stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?query=sweet potato&pageSize=10&api_key=#{ENV['FOOD_API_KEY']}").to_return(status: 200, body: response, headers: {})
 
-    sweet_potato_search = FoodFacade.search_by_title('sweet potato')
+    sweet_potato_search = FoodFacade.search('sweet potato')
     expect(sweet_potato_search).to be_an(Array)
     expect(sweet_potato_search.size).to eq(10)
     expect(sweet_potato_search.first).to be_an_instance_of(FoodPoro)
