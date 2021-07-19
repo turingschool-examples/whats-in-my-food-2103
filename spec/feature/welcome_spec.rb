081cfcdc4a24b6dfd0d70b4306e1c5f3dd656d0f
Richard <rdesilvey@gmail.com>
2021-07-19 09:34:07 -0600

    Write out feature test

--- spec/feature/welcome_spec.rb
+++ spec/feature/welcome_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'The welcome page' do
   describe 'search form' do
-    it 'navigates the user to the /food page'
+    it 'navigates the user to the /food page' do
+      visit '/'
+
+      within 'form' do
+        fill_in 'foods[q]', with: 'sweet potatoes'
+      end
+
+      expect(current_path).to eq foods_path
+    end
   end
 end
