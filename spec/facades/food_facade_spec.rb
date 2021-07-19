81813a179c4e4cd4dd5d1b871e4a53f0974d041e
Molly Krumholz <mkkrumholz@gmail.com>
2021-07-19 10:17:20 -0600

    Build out testing for FoodFacade

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -1,3 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe FoodFac
+RSpec.describe FoodFacade do
+  describe '.ingredient_search' do
+    it 'returns an array of foods with the searched ingredient' do
+      response_body = File.read('./spec/fixtures/sweet_potato_search.json')
+      stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?query=sweet potatoes&pageSize=10&api_key=#{ENV['api_key']}").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.5.1'
+           }).
+         to_return(status: 200, body: response_body, headers: {})
+
+      results = FoodFacade.ingredient_search('sweet potatoes')
+
+      expect(response).to be_an Array
+      expect(response.first).to be_a Food
+      expect(response.first.description).to eq 'SWEET POTATOES'
+      expect(response.first.brand).to eq "ARCHER FARMS"
+      expect(response.first.ingredients).to eq "SWEET POTATOES."
+      expect(response.first.upc_code).to eq "492111402857"
+    end
+  end
+end
--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe FoodService do
   describe '.search_by_ingredient' do
     it 'returns a list of foods with the searched ingredient' do
-      query = "sweet potatoes"
       response_body = File.read('./spec/fixtures/sweet_potato_search.json')
       stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?query=sweet potatoes&pageSize=10&api_key=#{ENV['api_key']}").
          with(
@@ -14,14 +13,16 @@ RSpec.describe FoodService do
            }).
          to_return(status: 200, body: response_body, headers: {})
 
-      response = FoodService.search_by_ingredient(query)
+      response = FoodService.search_by_ingredient("sweet potatoes")
 
       expect(response).to be_a Hash
       expect(response).to have_key :foods
       expect(response[:foods].length).to eq 10
       expect(response[:foods]).to be_an Array
       expect(response[:foods].first).to be_a Hash
+      expect(response[:foods].first[:description]).to eq "SWEET POTATOES"
       expect(response[:foods].first[:brandOwner]).to eq "ARCHER FARMS"
+      expect(response[:foods].first[:gtinUpc]).to eq "492111402857"
     end
   end
 end
