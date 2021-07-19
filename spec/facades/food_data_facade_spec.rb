210ee249a6315b146b1d7bf54362ceb2dbb3757b
Suzie K <suziekim.dev@gmail.com>
2021-07-19 12:02:38 -0400

    Red test returns list of food according to search

--- spec/facades/food_data_facade_spec.rb
+++ spec/facades/food_data_facade_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe FoodDataFacade do
 
     expect(fd).to be_a(FoodDataFacade)
   end
+
+  describe 'class methods' do
+    describe '::foods' do
+      it 'returns list of foods according to food query' do
+        foods = FoodDataFacade.foods("sweet potato")
+        first = foods.first
+   
+        expect(foods).to be_a(Array)
+        expect(foods.count).to eq(10)
+        expect(first.code).to eq('20042431')
+        expect(first.description).to eq('SWEET POTATO')
+        expect(first.ingredients.size).to eq(587)
+        expect(first.owner).to eq("FRESH & EASY")
+      end
+    end
+  end
 end 
