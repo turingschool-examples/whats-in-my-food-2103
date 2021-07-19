ad9321674d46b7a66ef4c50fa2c993acce10bebf
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-19 09:50:03 -0600

    Green food index path via search

--- spec/features/ing_search_spec.rb
+++ spec/features/ing_search_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'searching using ingredients' do
 
       fill_in('q', with: 'sweet potatoes')
       click_on('Search')
+
+      expect(page).to have_current_path('/foods', ignore_query: true)
     end
   end
 end
