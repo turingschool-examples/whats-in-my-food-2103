35d3c43bb748bbfb439f16c78017bd8d8a062f8a
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-07-19 10:39:11 -0600

    Green facade tests

--- app/facade/food_facade.rb
+++ app/facade/food_facade.rb
@@ -1,8 +1,9 @@
 class FoodFacade
-  def self.ingredient_search
-    json = 
-    json.map do |movie|
-      Movie.new(movie)
+  def self.ingredient_search(search_terms)
+    food_results = FoodService.food_search(search_terms)
+    food_array = food_results[:foods].map do |food|
+      Food.new(food)
     end
+    [food_results[:totalHits], food_array[0..9]]
   end
 end
--- app/poros/food.rb
+++ app/poros/food.rb
@@ -2,9 +2,9 @@ class Food
   attr_reader :gtin_upc, :description, :brand_owner, :ingredients
 
   def initialize(info_hash)
-    @gtin_upc = info_hash[:gtin_upc]
+    @gtin_upc = info_hash[:gtinUpc]
     @description = info_hash[:description]
-    @brand_owner = info_hash[:brand_owner]
+    @brand_owner = info_hash[:brandOwner]
     @ingredients = info_hash[:ingredients]
   end
 
