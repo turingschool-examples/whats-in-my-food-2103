d5d3ed779693d6aceef1202d89071f3aeda4e897
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-07-19 10:35:37 -0600

    finish FoodService

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,19 +1,26 @@
 class FoodService
 
+  def self.search(search)
+    response = connection.get('fdc/v1/foods/search') do |faraday|
+      faraday.params[:query] = search
+    end
+    parse_data(response)
+    # binding.pry
+  end
 
 
 
-# def conn
-#   Faraday.new(
-#     url: 'https://www.googleapis.com/civicinfo/v2',
-#     params: {
-#       'key' => (ENV['GOOGLE_CIVICS_API_KEY']).to_s
-#     }
-#   )
-# end
-#
-# def parse_data(response)
-#   JSON.parse(response.body, symbolize_names: true)
-# end
+  def self.connection
+    Faraday.new(
+      url: 'https://api.nal.usda.gov/',
+      params: {
+        'api_key' => ENV["food_api"]
+      }
+    )
+  end
+
+  def self.parse_data(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
 end
--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -5,9 +5,22 @@ RSpec.describe FoodService do
   describe "search for food info" do
 
     it "gets food info from api" do
+
+      test = File.read('spec/fixtures/sweet_potato.json')
+      stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=LP8KazhHMbzdDWoYbxshVgLsOG1VFoNDaWRgIDTz&query=sweet%20potato").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v1.5.1'
+          }).
+        to_return(status: 200, body: test, headers: {})
+
       response = FoodService.search("sweet potato")
 
       expect(response).to be_a(Hash)
+      expect(response[:foods]).to be_a(Array)
+      expect(response[:foods][0][:description]).to eq("SWEET POTATO")
     end
   end
 
