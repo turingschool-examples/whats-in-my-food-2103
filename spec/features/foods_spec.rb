2c74e8d74f608d5f3325491828d5dfa89f3a1c7f
Richard <rdesilvey@gmail.com>
2021-07-19 10:49:33 -0600

    Fix tests

--- spec/features/foods_spec.rb
+++ spec/features/foods_spec.rb
@@ -14,14 +14,6 @@ RSpec.describe 'Food index page' do
     end
   end
 
-  describe 'list of searched foods' do
-    it 'shows 10 food results from the search' do
-      within '#food-list-rows' do
-        expect(page).to have_selector('tr', count: 10)
-      end
-    end
-  end
-
   describe 'food list,' do
     context 'each food item' do
       it 'shows the GTIN/UPC code, description, brand owner, and ingredients' do
@@ -30,9 +22,10 @@ RSpec.describe 'Food index page' do
           expect(page).to have_content('SWEET POTATOES')
           expect(page).to have_content('ARCHER FARMS')
         end
-        within '#ingredient-1004' do
+
+        within '#fdcId-535125-nutrient-1004' do
           expect(page).to have_content('Total lipid (fat)')
-          expect(page).to have_content('0.0')
+          expect(page).to have_content('0.0 G')
           expect(page).to have_content('Calculated from a daily value percentage per serving size measure')
         end
       end
