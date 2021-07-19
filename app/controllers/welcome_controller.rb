086e13719fd91423e88ee4ca6dcdd86b8de903c8
Zach Green <zachjamesgreen@gmail.com>
2021-07-19 09:38:02 -0600

    get info to page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,20 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def foods
+    @q = params[:q]
+    res = Faraday.get('https://api.nal.usda.gov/fdc/v1/foods/search', {query: @q, api_key: ENV['USDA_FOOD_DATA_KEY']})
+    body = JSON.parse(res.body)
+    @total_hits = body['totalHits']
+    @foods = []
+    body['foods'].each do |f|
+      @foods << { 
+        gtin_upc: f['gtinUpc'],
+        brand_owner: f['brandOwner'],
+        description: f['description'],
+        ingredients: f['ingredients']
+      }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/foods', to: 'welcome#foods', as: 'foods'
+
   root "welcome#index"
 end
