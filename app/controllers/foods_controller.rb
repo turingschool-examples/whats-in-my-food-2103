12def8a22059b27b0f5b8a0d6fd394ef596dcc6e
ignored-comment <info.konham@gmail.com>
2021-07-19 11:58:46 -0400

    Return total search results for foods spec search

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,9 @@
 class FoodsController < ApplicationController
     def index
+        conn = Faraday.new(url: 'https://api.nal.usda.gov') do |faraday|
+            faraday.headers["X-API-KEY"] = ENV['food_data_key']
+        end
+        response = conn.get("/fdc/v1/foods/search?query=#{params[:q]}")
+        @results = JSON.parse(response.body, symbolize_names: true)
     end
 end
--- spec/features/food_search_spec.rb
+++ spec/features/food_search_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'Welcome Page' do
             fill_in :q, with: 'sweet potatoes'
             click_on "Search"
             expect(current_path).to eq('/foods')
+
+            expect(page).to have_content("45635 Results")
         end
     end
 end
