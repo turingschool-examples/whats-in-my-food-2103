3ff1289f25d731f95c320ecaec2f5e3557c6ddc8
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-19 09:39:39 -0700

    Add expect for robust testing

--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe FoodService do
   it 'can search for a food' do
     expect(FoodService.food_search("sweet potatoes").class).to eq(Hash)
-    require "pry";binding.pry
+    expect(FoodService.food_search("sweet potatoes")[:totalHits]).to eq(45635)
+    expect(FoodService.food_search("sweet potatoes")[:foods].count).to eq(50)
   end
 end
