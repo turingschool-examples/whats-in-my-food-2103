2da19ad46c9e56faef217ad59810b587ca364f8a
Suzie K <suziekim.dev@gmail.com>
2021-07-19 11:22:35 -0400

    Red test search_foods

--- spec/services/food_data_service_spec.rb
+++ spec/services/food_data_service_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe FoodDataService do
     expect(fds).to be_a(FoodDataService)
   end
 
+  describe 'class methods' do
+    context '::search_foods' do 
+      it 'returns list of foods that include the query keyword' do
+        VCR.use_cassette("sweet_potato_search") do
+          response = FoodDataService.search_foods("sweet potato")
+          require 'pry'; binding.pry
+          expect(response).to have_key(:total)
+          expect(response).to have_key(:limit)
+          expect(response[:data].size).to eq(15)
+        end
+      end
+    end
+  end
+
 end
