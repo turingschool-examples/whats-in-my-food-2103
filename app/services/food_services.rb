53f1e256740912ed36238901fcb94657700b3c36
JoannaCoPo <jococopo@gmail.com>
2021-07-19 10:15:03 -0600

    Set up food service

---
+++ app/services/food_services.rb
@@ -0,0 +1,18 @@
+class FoodService
+  def self.#METHOD NAME
+    response = conn.get("/fdc/v1/foods/search?api_key=#{ENV['PARKS_API_KEY']}&query=#{produce_item}")
+    parse_json(response)
+  end
+
+private
+
+  def self.conn
+    Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV['PARKS_API_KEY']
+    end
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
