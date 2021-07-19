82aabff3275566012af29a2b3b4cda20f7c894e7
Aliya Merali <merali4@gmail.com>
2021-07-19 10:49:44 -0600

    Add edge case test and functionality for result with no search

--- app/services/fdc_service.rb
+++ app/services/fdc_service.rb
@@ -4,5 +4,4 @@ class FdcService
     response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search?query=#{query}&pageSize=10&api_key=#{ENV['FDC_API_KEY']}")
     JSON.parse(response.body, symbolize_names: true)
   end
-
 end
--- spec/features/foods_search_spec.rb
+++ spec/features/foods_search_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe 'Food search results page' do
     stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?query=#{@query}&pageSize=10&api_key=#{ENV['FDC_API_KEY']}")
       .to_return(status: 200, body: response_body, headers: {})
 
+    @empty_query = "gibberish"
+    empty_body = File.read('spec/fixtures/empty_search.json')
+    stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?query=#{@empty_query}&pageSize=10&api_key=#{ENV['FDC_API_KEY']}")
+      .to_return(status: 200, body: empty_body, headers: {})
+
     visit root_path
   end
 
@@ -39,4 +44,17 @@ RSpec.describe 'Food search results page' do
 
     expect(page).to have_content("Chickpeas, from dried, fat added")
   end
+
+  it 'returns an empty page if no search results found' do
+    fill_in :q, with: @empty_query
+    click_button 'Search'
+
+    expect(page).to have_content("Total Results: 0")
+    expect(page).to_not have_content("GTIN/UPC Code")
+    expect(page).to_not have_content("Description")
+    expect(page).to_not have_content("Brand Owner")
+    expect(page).to_not have_content("Ingredients")
+
+  end
+
 end
